0d2b606be1050d40f93808f858e60871d9a6eed7
Mateusz Lewczak <leftarcode@gmail.com>
2022-11-06 16:20:19 +0100

    Change scene
    - Change scene to simple sphere
    - Delete Sponza scene

--- ptLeftarCore/ptLeftarCore.cpp
+++ ptLeftarCore/ptLeftarCore.cpp
@@ -22,43 +22,6 @@ std::vector<Material> materials;
 std::vector<Triangle> triangles;
 std::vector<Sphere> spheres;
 
-Color secondaryRay(Ray& ray, Octree& octree, int depth) {
-  if (depth <= 0) {
-    return Color{1, 1, 1};
-  }
-
-  Primitive::HitDescriptor hit;
-  if (octree.hit(ray, hit)) {
-    Vector3f target =
-        hit.position + hit.normal + Vector3f::randomInHemisphere(hit.normal);
-    Vector3f newPostion = hit.position;
-    Vector3f newDirection = target - hit.position;
-    newDirection.normalize();
-
-    Ray newRay;
-    newRay.origin = newPostion;
-    newRay.direction = newDirection;
-    newRay.direction.normalize();
-    newRay.invDirection =
-        Vector3f(1.0f / newRay.direction.x, 1.0f / newRay.direction.y,
-                 1.0f / newRay.direction.z);
-    newRay.invDirection.normalize();
-
-    Vector3f lightDirection = Vector3f(0.0f, -1.0f, -1.0f);
-    lightDirection.normalize();
-
-    Material currMat = materials[hit.materialId];
-    Color diffuseColor = currMat.diffuseColor;
-    if (currMat.diffuseTexture != nullptr) {
-      diffuseColor = currMat.diffuseTexture->sample(hit.uv);
-    }
-
-    return diffuseColor * secondaryRay(newRay, octree, depth - 1);
-  }
-
-  return Color{0, 0, 0};
-}
-
 void render(int width,
             int height,
             Camera& camera,
@@ -71,10 +34,17 @@ void render(int width,
       Ray r = camera.castRay(i, j);
 
       Color result{0, 0, 0};
-      for (int sample = 0; sample < 64; sample++) {
-        result += secondaryRay(r, octree, 4);
+      Primitive::HitDescriptor hit;
+      if (octree.hit(r, hit)) {
+        Material currMat = materials[hit.materialId];
+        result = currMat.diffuseColor;
+      } else {
+        Vector3f unitDirection = r.direction;
+        unitDirection.normalize();
+        float t = 0.5 * (unitDirection.y + 1.0);
+        result = Color(0, 0, 0) * (1.0 - t) + Color(0.5, 0.7, 1.0) * t;
       }
-      result /= 64;
+
       ImageColor color;
       color.r = result.r * 255;
       color.g = result.g * 255;
@@ -85,73 +55,23 @@ void render(int width,
 }
 
 int main() {
-  int width = 160;
-  int height = 160;
+  int width = 800;
+  int height = 400;
   data = (ImageColor*)malloc(sizeof(ImageColor) * width * height);
   memset(data, 0, sizeof(ImageColor) * width * height);
-
-  BoundingBox modelAABB(Vector3f(0, 0, 0), Vector3f(0, 0, 0));
-  objl::Loader Loader;
-  bool succes = Loader.LoadFile(
-      "crytek-sponza-huge-vray-obj/crytek-sponza-huge-vray.obj");
-  for (int i = 0; i < Loader.LoadedMeshes.size(); i++) {
-    objl::Mesh curMesh = Loader.LoadedMeshes[i];
-    std::vector<Vertex> vertices;
-    for (int j = 0; j < curMesh.Vertices.size(); j++) {
-      Vertex vertex;
-      vertex.position = Vector3f(curMesh.Vertices[j].Position.X,
-                                 curMesh.Vertices[j].Position.Y,
-                                 curMesh.Vertices[j].Position.Z);
-      vertex.normal =
-          Vector3f(curMesh.Vertices[j].Normal.X, curMesh.Vertices[j].Normal.Y,
-                   curMesh.Vertices[j].Normal.Z);
-      vertex.uv = Vector3f(curMesh.Vertices[j].TextureCoordinate.X,
-                           curMesh.Vertices[j].TextureCoordinate.Y, 0.0f);
-
-      vertices.push_back(vertex);
-    }
-
-    Material material;
-    material.diffuseColor.r = curMesh.MeshMaterial.Kd.X;
-    material.diffuseColor.g = curMesh.MeshMaterial.Kd.Y;
-    material.diffuseColor.b = curMesh.MeshMaterial.Kd.Z;
-    if (curMesh.MeshMaterial.map_Kd.length() > 0) {
-      Texture* diffuse = new Texture();
-      diffuse->loadFromFile("crytek-sponza-huge-vray-obj\\" +
-                            curMesh.MeshMaterial.map_Kd);
-      material.diffuseTexture = diffuse;
-    }
-    materials.push_back(material);
-
-    for (int j = 0; j < curMesh.Indices.size(); j += 3) {
-      Triangle triangle = Triangle(vertices[curMesh.Indices[j]],
-                                   vertices[curMesh.Indices[j + 1]],
-                                   vertices[curMesh.Indices[j + 2]]);
-      triangle.materialId = materials.size() - 1;
-
-      modelAABB.extend(vertices[curMesh.Indices[j]].position);
-      modelAABB.extend(vertices[curMesh.Indices[j + 1]].position);
-      modelAABB.extend(vertices[curMesh.Indices[j + 2]].position);
-      triangles.push_back(triangle);
-    }
-  }
-
-  SampledSpectrum::init();
-  SampledSpectrum spectrum;
-  RGBSpectrum rgbSpec;
 
   Material white;
   white.diffuseColor = Color{1, 1, 1};
   white.diffuseTexture = nullptr;
   materials.push_back(white);
 
-  Sphere sphere(modelAABB.center + Vector3f(-230, -400, 0), 50,
-                materials.size() - 1);
-  // spheres.push_back(sphere);
+  Sphere sphere(Vector3f(0, 0, 0), 50, materials.size() - 1);
+  spheres.push_back(sphere);
 
   Octree octree(triangles, spheres);
-  Camera camera(modelAABB.center + Vector3f(0, -300, 0), width, height, 45.0f);
+  Camera camera(Vector3f(120, 0, 0), width, height, 45.0f);
   camera.lookAt(camera.origin + Vector3f(-1, 0, 0));
+
   std::vector<std::thread> threads;
   threads.reserve(ThreadsCount);
   for (int i = 0; i < ThreadsCount; i++) {
@@ -163,7 +83,6 @@ int main() {
     threads[i].join();
   }
 
-  printf("RENDERED!\n");
   stbi_write_png("render.png", width, height, 3, data, 0);
   free(data);
 
